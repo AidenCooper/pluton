cf8a8cfe32d46e4c8f80d4ff9241774590606608
AidenCooper <aidencooper23@gmail.com>
2025-09-12 17:24:06 -0400

    Initial commit

---
+++ server/src/test/java/net/aidencooper/pluton/PlutonApplicationTests.java
@@ -0,0 +1,13 @@
+package net.aidencooper.pluton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlutonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
